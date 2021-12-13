a7c335934feb2d2c7b5ddab4af7417efa24ba0c3
kulveer16 <42886636+kulveer16@users.noreply.github.com>
2021-12-13 18:13:28 +0530

    new

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -6,5 +6,5 @@ cd /var/www/apps/hello-world
 
 # Starting the server...
 
-bash -lc "bundle exec rails server -d -p 80 -b 0.0.0.0"
-#bin/rails server -p 80
+#bash -lc "bundle exec rails server -d -p 80 -b 0.0.0.0"
+bin/rails server -p 80
